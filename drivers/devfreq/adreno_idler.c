d71ab9364d48c63d2fc81eb4b17a9a2b6454521b
[psychicgirl2020] <[anairachu@gmail.com]>
2020-06-21 14:04:54 +0000

    Fix compilation error

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -30,7 +30,6 @@
 
 #include <linux/module.h>
 #include <linux/devfreq.h>
-#include <linux/display_state.h>
 #include <linux/msm_adreno_devfreq.h>
 
 #define ADRENO_IDLER_MAJOR_VERSION 1
@@ -70,8 +69,6 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 {
 	if (!adreno_idler_active)
 		return 0;
-
-	display_on = is_display_on();
 
 	if (stats.busy_time < idleworkload) {
 		/* busy_time >= idleworkload should be considered as a non-idle workload. */
